6c3b9e54a7d6aebd3a81d25e289083faa342ea63
amschel99 <6290.2020@students.ku.ac.ke>
2023-02-20 13:29:53 +0300

    using docker compose for automation

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,6 @@
-#!/bin/bash
+echo "bringing down our service"
+ docker-compose down -v
 
-# Check if the stream-api:1.0 image exists locally
-echo "removing all containers"
-docker container prune -f
-echo "removing all volumes"
-docker volume prune
-if [[ "$(docker images -q stream-api:1.0 2> /dev/null)" == "" ]]; then
-  # If the image doesn't exist, build it
-  echo "Building stream-api:1.0 image"
-  docker build -t stream-api:1.0 .
-fi
+echo "using docker-compose up -d"
 
-# Check if a container called stream-app is running
-if [[ "$(docker ps -q -f name=stream-app)" != "" ]]; then
-  # If a container called stream-app is running, stop and delete it
-  echo "Stopping and deleting stream-app container"
-  docker stop stream-app
-  docker rm stream-app -fv # delete the container with the volume
-fi
-
-# Run the image in a new container
-echo "Starting stream-app container"
-docker run -v $(pwd):/app -v /app/node_modules --env-file .env -dp 8000:4000 --name stream-app stream-api:1.0
+docker-compose up -d
