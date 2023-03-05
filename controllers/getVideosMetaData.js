12d0c5deca0c0d21cf3ed82f1c02775a827f3815
amschel99 <6290.2020@students.ku.ac.ke>
2023-03-05 16:23:10 +0300

    bug-solved

--- controllers/getVideosMetaData.js
+++ controllers/getVideosMetaData.js
@@ -1,17 +1,17 @@
 import video from "../models/video.js"
 export const getVideosMetaData= async (req,res)=>{
     try{
-        const videos= video.find({apiKey:req.headers['apiKey']})
+        const videos= await video.find({apikey:req.headers['apikey']})
 res.json(videos)
     }
     catch(e){
 console.error(e.message)
     }
 }
-export const getVideoData=(req,res)=>{
+export const getVideoData=async (req,res)=>{
     const id=req.params.id
     try{
-const videoData= video.findOne({_id:id})
+const videoData= await video.findOne({_id:id})
         res.json(videoData)
     }
     catch(e){
