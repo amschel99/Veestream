2b380389bc1f2822e86e97c2925b603296e54b5e
amschel99 <6290.2020@students.ku.ac.ke>
2023-03-17 07:57:42 +0300

    bug fixing

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ echo "bringing down our service"
 echo "using docker-compose up -d"
 
 docker-compose up -d
-# useful curl command: curl -X POST -H "apiKey: b7642f0d6db02f383498e7f7edd353597ad8e5fe" -F "video=@/home/amschel/Downloads/honey.mp4" http://localhost:8000/video/upload
+# useful curl command: curl -X POST -H "apiKey: 59959534HX078772Rae8569f7c8bc5fafb6d7e0286b396bcc83551de4" -F "video=@/home/amschel/Downloads/honey.mp4" -F "name=thoughts"  http://localhost:8000/video/upload
